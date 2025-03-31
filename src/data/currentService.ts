dc5f1a7dd7140e935e5c6f643900b03eee64a59b
megpunyu <meg.punyu@meg.punyu.jp>
2025-03-31 22:58:28 +0900

    時刻表の修正

--- src/data/currentService.ts
+++ src/data/currentService.ts
@@ -1,5 +1,5 @@
 /**
- * 2024 second half
+ * 2025 first half
  * Timetable data of the bus service
  * between Hino and Minami-Osawa campuses
  */
@@ -8,55 +8,78 @@ import Service, { type Pattern } from "./Service";
 
 /**
  * @description
- * The service pattern for 2024 second half.
+ * The service pattern for 2025 first half.
  */
 const pattern: Pattern = {
-  // regular days
+  // normal days
   ...Object.fromEntries(
     (function* () {
-      const dayWisePatterns = [0, 2, 2, 4, 3, 2, 0];
-      const outOfService = new Date("2025-02-11"); // end of normal pattern
+      const dayWisePatterns = [0, 2, 2, 2, 2, 2, 0];
+      const startDate = new Date("2025-04-03"); // start of normal pattern
+      const endDate = new Date("2025-08-05"); // end of normal pattern
 
       // yield pattern ids
       for (
-        const date = new Date("2024-10-01");
-        date.getMonth() !== 2;
+        const date = startDate;
+        date.getMonth() !== 10;
         date.setDate(date.getDate() + 1)
       ) {
         const day = date.getDay();
         const key = date.toISOString().replace(/T.+/, "");
 
-        yield [key, date >= outOfService ? 0 : dayWisePatterns[day]];
+        yield [key, endDate < date ? 0 : dayWisePatterns[day]];
+      }
+    })()
+  ),
+
+  // exam or intensive lecture days
+  ...Object.fromEntries(
+    (function* () {
+      const startDate = new Date("2025-08-06");
+      const endDate = new Date("2025-09-12");
+
+      for (
+        const date = startDate;
+        date <= endDate;
+        date.setDate(date.getDate() + 1)
+      ) {
+        const key = date.toISOString().replace(/T.+/, "");
+
+        yield [key, 1];
+      }
+    })()
+  ),
+
+  // summer vacation
+  ...Object.fromEntries(
+    (function* () {
+      const startDate = new Date("2025-08-13");
+      const endDate = new Date("2025-08-31");
+
+      for (
+        const date = startDate;
+        date <= endDate;
+        date.setDate(date.getDate() + 1)
+      ) {
+        const key = date.toISOString().replace(/T.+/, "");
+
+        yield [key, 0];
       }
     })()
   ),
 
   // irregular days
-  "2024-10-31": 0,
+  "2025-04-29": 0, // Showa Day
 
-  "2024-11-01": 0,
-  "2024-11-04": 0,
+  "2025-05-05": 0, // Children's Day
+  "2025-05-06": 0, // substitute holiday for Greenery Day
 
-  "2024-12-24": 1,
-  "2024-12-25": 1,
-  "2024-12-26": 1,
-  "2024-12-30": 0,
-  "2024-12-31": 0,
-
-  "2025-01-01": 0,
-  "2025-01-02": 0,
-  "2025-01-03": 0,
-  "2025-01-13": 0,
-  "2025-01-17": 0,
-
-  "2025-02-05": 2,
-  "2025-02-11": 0,
-  "2025-02-12": 1,
-  "2025-02-13": 1,
-  "2025-02-14": 1,
+  "2025-08-11": 0, // Mountain Day
 };
 
+// debug
 console.log(pattern);
+
 /**
  * @description
  * Bus timetables from Hino to Minami-Osawa using pattern ids as key.
@@ -65,21 +88,21 @@ const hino = Service.createTimetable({
   0: {},
   1: {
     8: [[35, []]],
-    9: [[50, [2]]],
+    9: [[45, [2]]],
     12: [[20, [3]]],
     13: [[50, [4]]],
     15: [[20, [5]]],
-    16: [[30, [6]]],
-    18: [[30, [7]]],
+    17: [[0, [6]]],
+    18: [[45, [7]]],
   },
   2: {
     7: [[50, [1]]],
     8: [[35, []]],
     9: [
       [10, []],
-      [50, [2]],
+      [45, [2]],
     ],
-    10: [[30, []]],
+    10: [[40, []]],
     12: [[20, [3]]],
     13: [
       [0, []],
@@ -87,82 +110,15 @@ const hino = Service.createTimetable({
     ],
     14: [[40, []]],
     15: [[20, [5]]],
-    16: [
-      [20, []],
-      [30, [6]],
-    ],
+    16: [[20, []]],
+    17: [[0, [6]]],
     18: [
-      [0, []],
-      [30, [7]],
-    ],
-  },
-  3: {
-    7: [[50, [1]]],
-    8: [[35, []]],
-    9: [
-      [10, []],
-      [50, [2]],
-    ],
-    10: [[30, []]],
-    12: [
-      [20, [3]],
-      [50, []],
-    ],
-    13: [
-      [0, []],
-      [50, [4]],
-    ],
-    14: [
-      [40, []],
-      [45, []],
-    ],
-    15: [[20, [5]]],
-    16: [
-      [20, [10]],
-      [30, [6]],
-    ],
-    18: [
-      [0, []],
       [5, []],
-      [30, [7]],
+      [45, [7]],
     ],
   },
-  4: {
-    7: [[50, [1]]],
-    8: [
-      [35, []],
-      [40, []],
-    ],
-    9: [
-      [10, []],
-      [45, []],
-      [50, [2]],
-    ],
-    10: [[30, []]],
-    11: [[0, []]],
-    12: [
-      [20, [3]],
-      [50, []],
-    ],
-    13: [
-      [0, []],
-      [50, [4]],
-    ],
-    14: [
-      [40, []],
-      [45, []],
-    ],
-    15: [[20, [5]]],
-    16: [
-      [20, [10]],
-      [30, [6]],
-    ],
-    18: [
-      [0, []],
-      [5, []],
-      [30, [7]],
-    ],
-  },
+  3: {},
+  4: {},
 });
 
 /**
@@ -174,103 +130,36 @@ const minao = Service.createTimetable({
   1: {
     7: [[45, [1]]],
     9: [[10, [2]]],
-    10: [[35, [3]]],
+    10: [[40, [3]]],
     13: [[0, [4]]],
     14: [[40, [5]]],
-    15: [[55, [6]]],
-    17: [[30, [7]]],
+    16: [[25, [6]]],
+    18: [[5, [7]]],
   },
   2: {
     7: [[45, [1]]],
-    8: [[40, []]],
+    8: [[35, []]],
     9: [
       [10, []],
-      [50, [2]],
+      [45, [2]],
     ],
-    10: [[35, []]],
-    12: [[10, [3]]],
+    10: [[40, []]],
+    12: [[20, [3]]],
     13: [
       [0, []],
       [50, [4]],
     ],
     14: [[40, []]],
-    15: [
-      [25, [5]],
-      [55, []],
+    15: [[30, [5]]],
+    16: [[25, []]],
+    17: [[0, [6]]],
+    18: [
+      [5, []],
+      [45, [7]],
     ],
-    17: [
-      [10, [6]],
-      [30, []],
-    ],
-    18: [[45, [7]]],
   },
-  3: {
-    7: [[45, [1]]],
-    8: [[40, []]],
-    9: [
-      [10, []],
-      [50, [2]],
-    ],
-    10: [[35, []]],
-    12: [
-      [10, []],
-      [20, [3]],
-    ],
-    13: [
-      [0, []],
-      [45, []],
-      [50, [4]],
-    ],
-    14: [[40, []]],
-    15: [
-      [25, []],
-      [30, [5]],
-      [55, []],
-    ],
-    17: [
-      [0, []],
-      [10, [6]],
-      [30, []],
-    ],
-    18: [[45, [7]]],
-  },
-  4: {
-    7: [
-      [40, []],
-      [45, [1]],
-    ],
-    8: [[40, []]],
-    9: [
-      [10, []],
-      [15, []],
-      [50, [2]],
-    ],
-    10: [
-      [30, []],
-      [35, []],
-    ],
-    12: [
-      [10, []],
-      [20, [3]],
-    ],
-    13: [
-      [0, []],
-      [45, []],
-      [50, [4]],
-    ],
-    14: [[40, []]],
-    15: [
-      [25, []],
-      [30, [5]],
-      [55, []],
-    ],
-    17: [
-      [0, []],
-      [10, [6]],
-      [30, []],
-    ],
-    18: [[45, [7]]],
-  },
+  3: {},
+  4: {},
 });
 
 const service = new Service(pattern, { hino, minao });
